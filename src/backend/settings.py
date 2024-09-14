44218887daeb8600921a37b26e2850424151b4df
Muhammad Arif_Uddin <arifuddin.danin@gmail.com>
2024-09-14 19:09:56 +0500

    correct trusted origin

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -72,7 +72,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://airbnbbackend-production.up.railway.app",
     
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://airbnbbackend-production.up.railway.app',
+   # 'https://your-other-domain.com'  # Include other domains if necessary
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
