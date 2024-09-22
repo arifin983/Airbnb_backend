a4335100e6e4f4660e5129646fbe9bc0f20952a9
Muhammad Arif_Uddin <arifuddin.danin@gmail.com>
2024-09-22 19:52:44 +0500

    fixing redis connection

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -42,7 +42,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379/0')],
+           'hosts': [os.environ.get('REDIS_URL')],
         },
     },
 }
